7c81f5962f67f29df8fe0d26c66cc202916cf88f
yami_beta <yami-beta@users.noreply.github.com>
2017-06-17 17:11:41 +0900

    Add service worker

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -346,4 +346,11 @@
   }
 
   // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() {
+               console.log('Service Worker Registered');
+             });
+  }
 })();
